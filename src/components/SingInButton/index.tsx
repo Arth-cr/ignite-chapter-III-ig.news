9729455de6ba4418f6c8e378e1d83fd819f28d0f
Arth-cr <arthur.carvalho@outlook.com.br>
2022-01-15 23:39:57 -0300

    feat: backend with next and signIn github

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,17 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
-export function SingInButton() {
-  const isUserLoggedIn = true;
+import { signIn, useSession, signOut } from 'next-auth/react';
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+export function SingInButton() {
+  const { data: session } = useSession();
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Arthur Ribeiro
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
